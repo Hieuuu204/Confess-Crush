80c037aa736747920f3238c91ac11363f0ea2e17
Hieuuu204 <86715904+Hieuuu204@users.noreply.github.com>
2022-05-08 20:13:09 +0700

    Update config.js

--- config.js
+++ config.js
@@ -7,7 +7,7 @@ const CONFIG = {
     title: '    Chán m thật đấy đáng lẽ cái này sẽ không xuất hiện đâu bởi t muốn gặp trực tiếp để nói cơ, nhưng rủ cũng khó thật đấy-_-',
     desc: 'Do you want to be my girlfriend? ',
     btnYes: 'Cóo(ấn cái này đi:>)',
-    btnNo: 'Không( k ấn được đâu :>>) :3',
+    btnNo: 'Không( k ấn được đâu :>>)',
     question: 'Lúc này là t đang run và cũng phân vân lắm là có nên làm tiếp tục hay k đấy, nên là cho biết lí do sao đồng ý đi',
     btnReply: 'Đồng ý',
     messDesc: 'Nếu đồng ý rồi thì hãy call cho người ta đi bởi vì k dám nhìn vào màn hình nữa đâu:))',
